c5032d7c868aa887dd5da73b57c383641d8d3f74
Verbkin Mike <verbkinm@yandex.ru>
2021-10-19 20:53:43 +0500

    Update main.c

--- ДЗ/Алгоритмы и структуры данных/10/main.c
+++ ДЗ/Алгоритмы и структуры данных/10/main.c
@@ -55,6 +55,136 @@ bool check(char v)
     return check(v);
 }
 
+typedef struct Node
+{
+    int dat;
+    struct Node *next;
+} Node;
+
+typedef struct
+{
+    Node *head;
+    int size;
+} List;
+
+void init(List *list)
+{
+    list->head = NULL;
+    list->size = 0;
+}
+
+void insert(List *list, int data)
+{
+    Node *new = (Node*) malloc(sizeof (Node));
+    new->dat = data;
+    new->next = NULL;
+
+    Node *current = list->head;
+    if(!current)
+    {
+        list->head = new;
+        list->size++;
+    }
+    else
+    {
+        while (current->next != NULL)
+            current = current->next;
+
+        current->next = new;
+        list->size++;
+    }
+}
+
+Node* removeNode(List *list, int data)
+{
+    Node *current = list->head;
+    Node *parent = NULL;
+
+    if(current == NULL)
+        return NULL;
+
+    while (current->next != NULL && current->dat != data)
+    {
+        parent = current;
+        current = current->next;
+    }
+
+    if(current->dat != data)
+        return NULL;
+    if(current == list->head)
+    {
+        list->head = current->next;
+        list->size--;
+        return current;
+    }
+    parent->next = current->next;
+    list->size--;
+
+    return current;
+}
+
+void printNode(Node *node)
+{
+    if(node == NULL)
+    {
+        printf("[]");
+        return;
+    }
+    printf("[%d] ", node->dat);
+}
+
+void printList(List *list)
+{
+    Node *current = list->head;
+    if(current == NULL)
+        printNode(current);
+    else
+    {
+        do
+        {
+            printNode(current);
+            current = current->next;
+        }
+        while(current != NULL);
+    }
+    printf(" Size: %d \n", list->size);
+}
+
+List* copyList(List *list)
+{
+    List *newList = (List*) malloc(sizeof(List));
+    init(newList);
+
+    Node *current = list->head;
+
+    while (current != NULL)
+    {
+        insert(newList, current->dat);
+        current = current->next;
+    }
+
+    return newList;
+}
+
+bool isSorted(List *list)
+{
+    if(list->size == 0)
+        return false;
+    if(list->size == 1)
+        return true;
+
+    Node *current = list->head;
+    while (current->next != NULL)
+    {
+        if(current->dat > current->next->dat)
+            return false;
+
+        current = current->next;
+    }
+
+    return true;
+}
+
 int main()
 {
     char t;
